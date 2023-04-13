89dc2e9c712c95f1f9586165916892c0cb123e95
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2023-04-13 00:17:12 -0400

    Finished base challenge. Program could use some refactoring, cleanup, and TLC

--- Arrow.cs
+++ Arrow.cs
@@ -11,20 +11,61 @@ namespace FletchersArrows
 
         public string Fletching;
 
-        public Arrow(string fletching, string arrowhead, int shaftLength)
+        public Arrow(string arrowhead, int shaftLength, string fletching)
         {
+            Arrowhead = arrowhead;
             Fletching = fletching;
             ShaftLength = shaftLength;
-            Arrowhead = arrowhead;
         }
 
 
-        double CostPerArrow(Arrow customArrow)
+        public double CostPerArrow(Arrow customArrow)
         {
             double cost = 0;
-      
+            double arrowheadCost = 0;
+            double shaftCost = 0;
+            double fletchingCost = 0;
 
+            //Calculate Arrowhead cost
+            switch (customArrow.Arrowhead)
+            {
+                case "wood":
+                    arrowheadCost = 3;
+                    break;
+                case "obsidian":
+                    arrowheadCost = 5;
+                    break;
+                case "steel":
+                    arrowheadCost = 10;
+                    break;
+                default:
+                    arrowheadCost = -1;
+                    break;
+            }
+
+            //Calculate shaft cost
+            shaftCost = .05 * customArrow.ShaftLength;
+
+            //Calculate fletching cost
+            switch (customArrow.Fletching)
+            {
+                case "goose feathers":
+                    fletchingCost = 3;
+                    break;
+                case "turkey feathers":
+                    fletchingCost = 5;
+                    break;
+                case "plastic":
+                    fletchingCost = 10;
+                    break;
+                default:
+                    fletchingCost = -10999;
+                    break;
+            }
+
+            cost = arrowheadCost + shaftCost + fletchingCost;
             return cost;
+
         }
 
 
